62ee419fd4fd09274fe322984b6a00ffa02c2347
Akashsrivastava6 <akash.srivastava@ucdconnect.ie>
2019-07-30 23:27:26 +0100

    unit test

--- project_V1/core/Preprocess.py
+++ project_V1/core/Preprocess.py
@@ -3,61 +3,61 @@ import re
 from autocorrect import spell
   
 
-def preprocess(twt):
+# def preprocess(twt):
    
-   df_processed = pd.DataFrame()
-   df_processed['tweets'] = twt
-   df_processed['tweets']=remove_non_ascii(df_processed['tweets'])
-   df_processed['tweets']=remove_newline(df_processed['tweets'])
-   df_processed['tweets']=remove_punctuation(df_processed['tweets'])
-   #df_processed['tweets']=remove_unwanted_spaces(df_processed['tweets'])
-   for i in range(len(df_processed['tweets'])):
-      df_processed.loc[i]['tweets']=remove_unwanted_spaces(df_processed.loc[i]['tweets'])
-   tweet,ind=remove_emoticon_replacement(df_processed)
+#    df_processed = pd.DataFrame()
+#    df_processed['tweets'] = twt
+#    df_processed['tweets']=remove_non_ascii(df_processed['tweets'])
+#    df_processed['tweets']=remove_newline(df_processed['tweets'])
+#    df_processed['tweets']=remove_punctuation(df_processed['tweets'])
+#    #df_processed['tweets']=remove_unwanted_spaces(df_processed['tweets'])
+#    for i in range(len(df_processed['tweets'])):
+#       df_processed.loc[i]['tweets']=remove_unwanted_spaces(df_processed.loc[i]['tweets'])
+#    tweet,ind=remove_emoticon_replacement(df_processed)
 
-   return tweet,ind
+#    return tweet,ind
 
-def remove_non_ascii(dff):
-    #s2 = str(df) 
-   s2 = dff.str.replace("[^\\x00-\\x7F]", "abcde")
-   return s2
+# def remove_non_ascii(dff):
+#     #s2 = str(df) 
+#    s2 = dff.str.replace("[^\\x00-\\x7F]", "abcde")
+#    return s2
 
 
 
-def remove_newline(dff):
-     #s2 = str(df) 
-   s2= dff.str.replace("\r\n", " ")
-   return s2
+# def remove_newline(dff):
+#      #s2 = str(df) 
+#    s2= dff.str.replace("\r\n", " ")
+#    return s2
   
 
 
-def remove_punctuation(df):
-    #s2 = str(df) 
-   s2=df.str.replace('[^\w\s]',' ')
-   return s2
+# def remove_punctuation(df):
+#     #s2 = str(df) 
+#    s2=df.str.replace('[^\w\s]',' ')
+#    return s2
 
 
-# Creating a function remove_unwanted_spaces() to remove all unwanted spaces
-def remove_unwanted_spaces(s):
-   s= re.sub('\s+',' ',s)
-   s=s.strip() # the strip() function removes any trailing spaces from the begining snd the end of the text
-   return s
+# # Creating a function remove_unwanted_spaces() to remove all unwanted spaces
+# def remove_unwanted_spaces(s):
+#    s= re.sub('\s+',' ',s)
+#    s=s.strip() # the strip() function removes any trailing spaces from the begining snd the end of the text
+#    return s
  
 
 
 
-def remove_emoticon_replacement(df):
-   tweets=[]
-   ind=[]
-   j=0
-   for i in range(len(df['tweets'])):
-      x = re.search("(abcde)",df.loc[i]['tweets'])
-      if (x):
-         ind.append(i)
-         tweets.append("abcde")
-      else:
-         tweets.append(df.loc[i]['tweets'])
-   return tweets,ind
+# def remove_emoticon_replacement(df):
+#    tweets=[]
+#    ind=[]
+#    j=0
+#    for i in range(len(df['tweets'])):
+#       x = re.search("(abcde)",df.loc[i]['tweets'])
+#       if (x):
+#          ind.append(i)
+#          tweets.append("abcde")
+#       else:
+#          tweets.append(df.loc[i]['tweets'])
+#    return tweets,ind
 # df_processed = pd.DataFrame()
 # df_processed['tweets'] = tweets
 
--- project_V1/core/test/test_view.py
+++ project_V1/core/test/test_view.py
@@ -1,14 +1,185 @@
 from django.test import TestCase,Client
 from django.urls import reverse
-#from core.models import following,tweets_data,notification_data
-import json
+from core.models import following,tweets_data,notification_data
+from login.models import User_detail,Registration
 
-# class TestCoreViews(TestCase):
+class TestCoreViews(TestCase):
+    def test_twitter_check_3(self):
+       
+        client=Client()
+        response=client.post(reverse("twitter_check"))
 
-#     def test_addfriend(self):
+        self.assertEquals(response.status_code,302)
+        
 
-#         client=Client()
-#         response=client.get(reverse("addfriend"))
 
-#         self.assertEquals(response.status_code,302)
-#         self.assertTemplateUsed(response,"login")
+    def test_followers_check1(self):
+        client=Client()
+        response=client.get(reverse("follower"))
+        self.assertEquals(response.status_code,302)
+
+
+    def test_follower_check2(self):
+        client=Client()
+        session=client.session
+        session['username']="Akashsri"
+        session.save()
+
+        response=client.post(reverse("follower"),{
+            "friend":"buddyupucd",
+            "status":"Grant"
+        })
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,"followers.html")
+
+    def test_follower_check3(self):
+        client=Client()
+        session=client.session
+        session['username']="Akashsri"
+        session.save()
+
+        response=client.post(reverse("follower"),{
+            "friend":"buddyupucd",
+            "status":"Revoke"
+        })
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,"followers.html")
+    
+    def test_trend_check1(self):
+        client=Client()
+        response=client.get(reverse("trend"))
+        self.assertEquals(response.status_code,302)    
+
+    def test_trend_check2(self):
+        following.objects.create(
+            user_id="Akashsri",
+            twitter_handle="buddyupucd",
+            friend_Email="abc@gmail.com",
+            url=",",
+            isActive=1
+        )
+        client=Client()
+        session=client.session
+        session['username']="Akashsri"
+        session.save()
+        response=client.post(reverse("trend"),{
+            "friend":"buddyupucd"
+        })
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,"trend.html")
+
+
+    def test_trend_check3(self):
+        
+        client=Client()
+        session=client.session
+        session['username']="Akashsri"
+        session.save()
+        response=client.post(reverse("trend"))
+
+        self.assertEquals(response.status_code,302)
+        #self.assertTemplateUsed(response,"trend.html")
+
+
+
+
+    def test_addfriend(self):
+        client=Client()
+        session=client.session
+        session['username']="Akashsrivastava6"
+        session.save()
+        following.objects.create(
+            user_id=session['username'],
+            twitter_handle="buddyupucd",
+            friend_Email="Akashsrivastava6@gmail.com",
+            url="",
+            isActive=1
+        )
+        response=client.post(reverse("addfriend"),{
+            "twitter_handle":"buddyupucd",
+            "email":"Akashsrivastava6@gmail.com"})
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,"dashboard.html")
+
+
+    def test_addfriend_without_session(self):
+        client=Client()
+       
+        response=client.post(reverse("addfriend"),{
+            "twitter_handle":"buddyupucd",
+            "email":"Akashsrivastava6@gmail.com"})
+
+        self.assertEquals(response.status_code,302)
+        #self.assertTemplateUsed(response,"login")
+
+
+    def test_twitter_check(self):
+        User_detail.objects.create(
+            username="Akashsrivastava6",
+        )
+        client=Client()
+        session=client.session
+        session['username']="Akashsrivastava6"
+        session.save()
+        response=client.post(reverse("twitter_check"),{
+            "username":"Akashsrivastava6@gmail.com",
+            "password":"Akashsri",
+            "fname":"Akash",
+            "lname":"Srivastava",
+            "dob":"1992-03-03"
+        })
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,"dashboard.html")
+
+
+
+    def test_twitter_check_1(self):
+        User_detail.objects.create(
+            username="Akashsrivastava6",
+        )
+        following.objects.create(
+            user_id="Akashsrivastava",
+            twitter_handle="Akashsrivastava6",
+            friend_Email="abs@gmail.com",
+            url="",
+            isActive=1
+        )
+        client=Client()
+        session=client.session
+        session['username']="Akashsrivastava6"
+        session.save()
+        response=client.post(reverse("twitter_check"),{
+            "username":"Akashsrivastava6@gmail.com",
+            "password":"Akashsri",
+            "fname":"Akash",
+            "lname":"Srivastava",
+            "dob":"1992-03-03"
+        })
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,"dashboard.html")   
+
+    # def test_twitter_check_2(self):
+       
+    #     client=Client()
+    #     session=client.session
+    #     session['username']="Akashsrivastava6"
+    #     session.save()
+    #     response=client.post(reverse("twitter_check"),{
+    #         "username":"Akashsrivast@gmail.com",
+    #         "password":"Akashsri",
+    #         "fname":"Akash",
+    #         "lname":"Srivastava",
+    #         "dob":"1992-03-04"
+    #     })
+
+    #     self.assertEquals(response.status_code,200)
+    #     self.assertTemplateUsed(response,"dashboard.html")   
+
+
+    
